2b9e86b6647e8aa691230e14e2b4cfa060b6dd85
BlackElias <valienne.elias@outlook.com>
2021-04-22 17:17:12 +0200

    homepage frontend done

--- home.php
+++ home.php
@@ -33,9 +33,79 @@
     </div>
 </nav>
 <body>
-  <h1 class="wrapper title">Needit! Leer en leen met je buren</h1>
-  <h3 class="wrapper">Top markt platform voor u en uw buren <br>Lennen gaat nu nog gemakkelijker!</h3>  
-  <img src="img/" alt="">
+  <h1 class="wrapper title">Needit! <br id="title"> Leer en leen met je buren</h1>
+  <h3 class="wrapper title-text">Top markt platform voor u en uw buren <br>Lennen gaat nu nog gemakkelijker!</h3>  
+  <img src="img/hero-img.png" class="img-fluid title-img" alt="background image">
+  <h1 class="wrapper title title-maxScreen">Hoe doet Needit dit?</h1>
+  <p  class="wrapper text-mobile text-maxScreen">Needit biedt een markt platform voor buren om goederen te
+kunnen lenen op een bepaalde datum die je ingeeft. Zo gaan
+we de circulaire cirkel doorbreken. Door hergebruiken van 
+producten en ervoor zorgen voor minder onnodige aankopen.
+
+De hele wereld laten delen is nogal een opgave.
+Dat doe je niet even op een namiddag. Het kost veel tijd, geld 
+en inspanning. Maar vaak zijn dingen die moeilijk zijn, juist de
+moeite waard.</p>
+
+  <img src="img/Mockup.png" alt="mockup" class="mockup" >
+
+
+
+  <div class="grey wrapper">
+  <form method="POST" class="wrapper-medium"> 
+  <div class="mb-3 font">
+      <h1 class="title-left">Meld je aan!</h1>
+    <label for="username" class="form-label">Gebruikersnaam</label>
+    <input type="name" class="form-control black-border" style="border-radius: 10px;" id="username" >
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+
+  <div class="mb-3 font ">
+    <label for="exampleInputPassword1" class="form-label">Email</label>
+    <input type="email" class="form-control black-border" style="border-radius: 10px;" id="exampleInputPassword1">
+  </div>
+
+  <div class="mb-3 font ">
+    <label for="exampleInputEmail1" class="form-label">Passwoord</label>
+    <input type="email" class="form-control black-border" style="border-radius: 10px;" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
+  </div>
+
+  <div class="mb-3 font ">
+    <label for="exampleInputEmail1" class="form-label">Straatnaam</label>
+    <input type="ship-address" class="form-control black-border" style="border-radius: 10px;" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
+  </div>
+
+  <div class="mb-3 font float-end">
+    <label for="exampleInputEmail1" class="form-label">Nummer</label>
+    <input type="email" class="form-control black-border w-50" style="border-radius: 10px;" id="exampleInputEmail1" aria-describedby="emailHelp">
+    
+  </div>
+
+  <div class="mb-3 font ">
+    <label for="exampleInputEmail1" class="form-label">Postcode</label>
+    <input type="ship-zip" class="form-control black-border w-25" style="border-radius: 10px;" id="exampleInputEmail1" aria-describedby="ship-zip">
+    
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+ 
+  
+
+
+ 
+  <h1 class="title">Mission statement</h1>
+  <p class="text-mobile">Vijfduizend euro voor spullen die je nauwelijks gebruikt. Neem bijvoorbeeld een gemiddelde boormachine; die wordt maar zo’n 15 minuten gebruikt tijdens zijn hele levensduur. En dat terwijl zo’n boor wel 8 uur aan een stuk kan boren. 
+
+Dat is alsof je een 10-literpak melk moet kopen terwijl je maar een glaasje wilt drinken. Als we een boormachine delen kunnen er maar liefst 32 mensen plezier van hebben in. En dat scheelt een hoop.</p>
+  <h1 class="title">Over ons</h1>
+  <p class="text-mobile">Needit is een markt platform gemaakt door 2 studenten van Thomas more. Om mensen weer sociaal contact te zoeken door goederen  en diensten te kunnen lenen tussen buren of onbekende. Hierdoor pakken we ook 
+circulaire economie aan en gaan we producten hun levensduur meer gebruiken.
+
+Needit is met geen enkele politieke partij of andere belanghebbers verbonden en We hebben nog geen samenwerkingen. </p>
+</form>
+  </div>
+  <img src="img/background.png" alt="background" class="img-fluid background">
 </body>
 <footer class="footer bg-light text-center text-lg-start">
         <!-- Copyright -->
