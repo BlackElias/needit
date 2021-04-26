8dd27d57fe392595f231b2c7bda288b449adb545
BlackElias <valienne.elias@outlook.com>
2021-04-26 16:30:49 +0200

    index page

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
     <link rel="stylesheet" href="app.css">
+    <link rel="stylesheet" href="https://use.typekit.net/hay5vbn.css">
+   
   <title>Document</title>
 </head>
 <!-- Navbar -->
@@ -61,9 +63,11 @@
 
 
         <div class="col-md col-mobile">
-        <h1>articles</h1>
-      
-        </div>
+        
+        <div  style= "margin-left: 39%; margin-top: 2%">
+      <a href="" class="finders " style="text-decoration: none;  color:black; margin-right:50px;">Artikelen  </a><a href="" class="finders" style="text-decoration: none; color:grey ;">Zoekertjes</a>
+       <hr style="margin-right: 73%; marging-top: 0px">
+    </div></div>
     </div>
   </div>
 </body>
